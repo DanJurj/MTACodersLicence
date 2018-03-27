de5e089e95e357eda851fe469bb6a838593ad4ae
dan jj <dan.jurj19@gmail.com>
2018-03-27 21:30:15 +0300

    Add project files.

---
+++ MTACodersLicence/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MTACodersLicence.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
